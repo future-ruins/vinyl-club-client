5250995b6a78efacb177b762fd43fc36cb21d8bc
future-ruins <helliot885@gmail.com>
2019-11-26 20:08:11 +0100

    Add RecordsBoard presentational component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import LoginFormContainer from './LoginFormContainer';
 import SignupFormContainer from './SignupFormContainer';
+import RecordsBoard from './RecordsBoard';
 
 export default class Homepage extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Homepage extends Component {
         <main>
           <Route path='/login' component={LoginFormContainer} />
           <Route path='/signup' component={SignupFormContainer} />
+          <Route path='/records-board' component={RecordsBoard} />
         </main>
       </BrowserRouter>
     );
