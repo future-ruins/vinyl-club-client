ba2173341bdf80538e65301ad0d5a9b1fd82ad70
future-ruins <helliot885@gmail.com>
2019-12-05 15:56:03 +0100

    Display record info on recordPgae component

--- src/components/RecordPage.js
+++ src/components/RecordPage.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
 class RecordPage extends Component {
   render() {
-    return <p>Record</p>;
+    return (
+      <>
+        <Container maxWidth='sm'>
+          <Grid
+            container
+            direction='column'
+            justify='center'
+            spacing={14}
+            alignItems='center'>
+            <img src={this.props.selectedRecord.img} alt='record cover'></img>
+
+            <Typography
+              component='h1'
+              variant='h2'
+              align='center'
+              color='textPrimary'
+              gutterBottom>
+              {this.props.selectedRecord.title}
+            </Typography>
+
+            <Typography
+              variant='body2'
+              align='center'
+              color='textSecondary'
+              paragraph>
+              Format: {`${this.props.selectedRecord.format}"`}
+              <br />
+              Genre: {this.props.selectedRecord.genre}
+            </Typography>
+
+            <Typography
+              variant='body2'
+              align='center'
+              color='texttextPrimary'
+              paragraph>
+              {this.props.selectedRecord.description}
+            </Typography>
+          </Grid>
+        </Container>
+      </>
+    );
   }
 }
 
