55d3f886b8ec1aee478bdb8799871118021795a5
future-ruins <helliot885@gmail.com>
2019-12-06 11:34:40 +0100

    Style recordPage component

--- src/components/RecordPage.js
+++ src/components/RecordPage.js
@@ -1,49 +1,46 @@
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 
 class RecordPage extends Component {
   render() {
     return (
       <>
-        <Container maxWidth='sm'>
-          <Grid
-            container
-            direction='column'
-            justify='center'
-            spacing={14}
-            alignItems='center'>
-            <img src={this.props.selectedRecord.img} alt='record cover'></img>
+        <img
+          class='centeredImage'
+          src={this.props.selectedRecord.img}
+          alt='record cover'
+          height={300}
+          position='center'></img>
+        <br />
+        <br />
+        <Typography
+          component='h1'
+          variant='h2'
+          align='center'
+          color='textPrimary'
+          gutterBottom>
+          {this.props.selectedRecord.title}
+        </Typography>
 
-            <Typography
-              component='h1'
-              variant='h2'
-              align='center'
-              color='textPrimary'
-              gutterBottom>
-              {this.props.selectedRecord.title}
-            </Typography>
+        <Typography
+          variant='body2'
+          align='center'
+          color='textSecondary'
+          paragraph>
+          Format: {`${this.props.selectedRecord.format}"`}
+          <br />
+          Genre: {this.props.selectedRecord.genre}
+          <br />
+          Posted by: {this.props.selectedRecord.userId}
+        </Typography>
 
-            <Typography
-              variant='body2'
-              align='center'
-              color='textSecondary'
-              paragraph>
-              Format: {`${this.props.selectedRecord.format}"`}
-              <br />
-              Genre: {this.props.selectedRecord.genre}
-            </Typography>
-
-            <Typography
-              variant='body2'
-              align='center'
-              color='texttextPrimary'
-              paragraph>
-              {this.props.selectedRecord.description}
-            </Typography>
-          </Grid>
-        </Container>
+        <Typography
+          variant='body2'
+          align='center'
+          color='textPrimary'
+          paragraph>
+          {this.props.selectedRecord.description}
+        </Typography>
       </>
     );
   }
